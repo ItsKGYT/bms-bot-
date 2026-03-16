95fa2e0157778f6b8a42dd7fc5047f109563e193
ItsKGYT <41182286+ItsKGYT@users.noreply.github.com>
2026-03-16 16:36:57 +0530

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import time
 import requests
-from playwright.sync_api import sync_playwright
-
-URL = "https://in.bookmyshow.com/cinemas/hyderabad/allu-cinemas-kokapet/ALUC"
+import time
 
 BOT_TOKEN = "8640561400:AAFdvFX70zsngNUEL7KOUDZ0d07pwgKwx68"
 CHAT_ID = "410880894"
 
+URL = "https://in.bookmyshow.com/cinemas/hyderabad/allu-cinemas-kokapet/ALUC"
+
 def send_telegram():
-    message = "🎟 Tickets for March 21 are LIVE!\nhttps://in.bookmyshow.com/cinemas/hyderabad/allu-cinemas-kokapet/ALUC"
+    message = "🎟 Tickets for March 21 are LIVE!\n" + URL
 
     requests.get(
         f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
@@ -19,25 +18,20 @@ def send_telegram():
     )
 
 while True:
-    print("Opening browser...")
+    print("Checking BookMyShow API...")
 
-    with sync_playwright() as p:
-        browser = p.chromium.launch(args=["--no-sandbox"])
-        page = browser.new_page()
-
-        page.goto(URL)
-        page.wait_for_timeout(5000)
-
-        html = page.content()
+    try:
+        r = requests.get(URL)
+        html = r.text
 
         if "20 Mar" in html:
             print("21 detected!")
             send_telegram()
-            browser.close()
             break
         else:
             print("21 not available")
 
-        browser.close()
+    except Exception as e:
+        print("Error:", e)
 
     time.sleep(60)

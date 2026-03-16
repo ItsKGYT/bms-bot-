74d72d862c208920cac3dc18755307666512f300
ItsKGYT <41182286+ItsKGYT@users.noreply.github.com>
2026-03-16 16:05:07 +0530

    main.py

--- main.py
+++ main.py
@@ -3,23 +3,35 @@ import time
 
 URL = "https://in.bookmyshow.com/cinemas/hyderabad/allu-cinemas-kokapet/ALUC"
 
-BOT_TOKEN = "8640561400:AAFDvFX70zsngNUEL7KOUDZod07pwgKwz68"
-CHAT_ID = "410880894"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+CHAT_ID = "YOUR_CHAT_ID"
 
-def notify():
+def send_telegram():
+    message = "🎟 Tickets for March 21 are LIVE!\nhttps://in.bookmyshow.com/cinemas/hyderabad/allu-cinemas-kokapet/ALUC"
+
     requests.get(
         f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
         params={
             "chat_id": CHAT_ID,
-            "text": "🎟 Tickets for March 21 are LIVE!\nhttps://in.bookmyshow.com/cinemas/hyderabad/allu-cinemas-kokapet/ALUC"
-        },
+            "text": message
+        }
     )
 
 while True:
-    r = requests.get(URL)
+    print("Checking page...")
 
-    if "21" in r.text:
-        notify()
-        break
+    try:
+        r = requests.get(URL)
+
+        if "21" in r.text:
+            print("21 detected!")
+            send_telegram()
+            break
+
+        else:
+            print("21 not available")
+
+    except Exception as e:
+        print("Error:", e)
 
     time.sleep(3)
